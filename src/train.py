742abefb517b81d04e3e5dbcf7d969d5aa213ef6
Skyler-18 <hardik.goriwan@gmail.com>
2025-06-30 23:47:31 +0530

    Checked it with some changes in train.py

--- src/train.py
+++ src/train.py
@@ -10,7 +10,7 @@ X = df.drop("target", axis=1)
 y = df["target"]
 
 # Train-test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Train model
 model = RandomForestClassifier()
